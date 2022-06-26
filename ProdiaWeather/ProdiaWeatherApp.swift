c259322a436bbe5b57629ec169889e7d44c3367e
Muhamad Jakfar Shodiq <jakfarsoo@gmail.com>
2022-06-26 10:56:29 +0700

    Initial Commit

---
+++ ProdiaWeather/ProdiaWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProdiaWeatherApp.swift
+//  ProdiaWeather
+//
+//  Created by Administrator  on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct ProdiaWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
